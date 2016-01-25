4a50a5487faf76821fc8ef12d10a41db329f69e6
Seok Kang <grtlinux@gmail.com>
2016-01-26 03:12:26 +0900

    create project

---
+++ KANG01/src/tain/kr/com/kang01/hello/Hello01.java
@@ -0,0 +1,9 @@
+package tain.kr.com.kang01.hello;
+
+public class Hello01 {
+
+	public static void main(String[] args) throws Exception
+	{
+		System.out.println("Hello, world!!! KANG01");
+	}
+}
---
+++ KANG02/src/tain/kr/com/kang02/hello/Hello02.java
@@ -0,0 +1,9 @@
+package tain.kr.com.kang02.hello;
+
+public class Hello02 {
+
+	public static void main(String[] args) throws Exception
+	{
+		System.out.println("Hello, world!!! KANG02");
+	}
+}
