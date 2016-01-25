58e5558b273ffec882dffac4032412ed65570385
Seok Kang <grtlinux@gmail.com>
2016-01-26 04:02:05 +0900

    add log4j

--- KANG01/src/tain/kr/com/kang01/hello/Hello01.java
+++ KANG01/src/tain/kr/com/kang01/hello/Hello01.java
@@ -1,9 +1,17 @@
 package tain.kr.com.kang01.hello;
 
+import org.apache.log4j.Logger;
+
 public class Hello01 {
+	
+	private static boolean flag = true;
+	
+	private static final Logger log = Logger.getLogger(Hello01.class);
 
 	public static void main(String[] args) throws Exception
 	{
-		System.out.println("Hello, world!!! KANG01");
+		// System.out.println("Hello, world!!! KANG01");
+		
+		if (flag) log.debug("Hello, world!!! KANG01");
 	}
 }
