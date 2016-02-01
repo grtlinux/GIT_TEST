0db778c736ed1be06afd9844d97e27973d9f232b
Seok Kang <grtlinux@gmail.com>
2016-02-01 16:10:19 +0900

    .

---
+++ KANG01/src/tain/kr/com/test/enumtest/v02/EnumClient.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.enumtest.v02;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   1. ClassName  : 
+ *   2. FileName   : EnumClient.java
+ *   3. Package    : tain.kr.com.test.enumtest.v02
+ *   4. Comment    :
+ *   5. Author     : taincokr
+ *   6. First Date : 2016. 2. 1. ¿ÀÈÄ 3:58:40
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+public class EnumClient {
+
+}
