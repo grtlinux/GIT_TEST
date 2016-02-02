a880131473fdff1b7cace3f83ee037c5bd6eee74
Seok Kang <grtlinux@gmail.com>
2016-02-02 13:01:24 +0900

    .

--- KANG01/src/tain/kr/com/test/file/v01/FileTestMain.java
+++ KANG01/src/tain/kr/com/test/file/v01/FileTestMain.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
 package tain.kr.com.test.file.v01;
 
 import java.io.File;
@@ -7,6 +26,21 @@ import java.util.Date;
 
 import org.apache.log4j.Logger;
 
+/**
+ * 
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : FileTestMain.java
+ *   -. Package    : tain.kr.com.test.file.v01
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 2. 2. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
 public class FileTestMain {
 
 	final static Logger log = Logger.getLogger(FileTestMain.class);
