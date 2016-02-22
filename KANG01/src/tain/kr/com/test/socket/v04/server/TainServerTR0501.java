1949dae2d8ecd53abd2dacb33fd0b2d308795a86
Seok Kang <grtlinux@gmail.com>
2016-02-22 12:21:06 +0900

    .

---
+++ KANG01/src/tain/kr/com/test/socket/v04/server/TainServerTR0501.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright 2014, 2015, 2016 TAIN, Inc. all rights reserved.
+ *
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * -----------------------------------------------------------------
+ * Copyright 2014, 2015, 2016 TAIN, Inc.
+ *
+ */
+package tain.kr.com.test.socket.v04.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
+import org.apache.log4j.Logger;
+
+import tain.kr.com.test.socket.v04.common.Exec;
+import tain.kr.com.test.socket.v04.common.PacketHeader;
+
+/**
+ * Code Templates > Comments > Types
+ *
+ * <PRE>
+ *   -. FileName   : TainServerThread.java
+ *   -. Package    : tain.kr.com.test.socket.v02
+ *   -. Comment    :
+ *   -. Author     : taincokr
+ *   -. First Date : 2016. 2. 16. {time}
+ * </PRE>
+ *
+ * @author taincokr
+ *
+ */
+@SuppressWarnings("unused")
+public class TainServerTR0501 {
+
+	private static boolean flag = true;
+
+	private static final Logger log = Logger.getLogger(TainServerTR0501.class);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	
+	private Socket socket = null;
+	private DataInputStream dis = null;
+	private DataOutputStream dos = null;
+	private byte[] packet = null;
+
+	public TainServerTR0501(Socket socket, DataInputStream dis, DataOutputStream dos, byte[] packet) throws Exception {
+		
+		if (flag) {
+			this.socket = socket;
+			this.dis = dis;
+			this.dos = dos;
+			this.packet = packet;
+		}
+	}
+	
+	public byte[] execute() throws Exception {
+	
+		if (flag) {
+			/*
+			 * execute shell program
+			 */
+			//Exec.run(new String[] {"cmd", "/c", "dir"});
+			Exec.run(new String[] {"cmd", "/c", "start"}, false);
+		}
+		
+		if (flag) {
+			PacketHeader.TR_CODE.setVal(this.packet, "TR0501");
+			PacketHeader.RET_CODE.setVal(this.packet, "00000");
+			PacketHeader.FILLER.setVal(this.packet, "SUCCESS");
+		}
+		
+		return this.packet;
+	}
+	
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////////////////////////
+}
